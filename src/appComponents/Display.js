00a64e836fdcc478175fd6a4e8109f91665bbdf8
vicky <vzhong2021@gmail.com>
2022-07-12 14:56:48 -0400

    fixed display for multiple classes

--- src/appComponents/Display.js
+++ src/appComponents/Display.js
@@ -1,28 +1,15 @@
 const Display = ({exchanges, newFind, newExchange}) => {
   const contains = (current, find, exchange) => {
-    if (find === null) {
-      find = ''; 
-    }
-    if (exchange === null) {
-      exchange = '';
-    }
-    if (current.finding === null) {
-      current.finding = '';
-    }
-    if (current.exchanging === null) {
-      current.exchanging = ''; 
-    }
-    let currFind = current.finding.toUpperCase().trim(); 
-    let currExchange = current.exchanging.toUpperCase().trim(); 
-    find = find.toUpperCase().trim(); 
-    exchange = exchange.toUpperCase().trim(); 
-    return currFind.includes(find) && currExchange.includes(exchange)
+    find = (find === null) ? '' : find.toUpperCase().trim(); 
+    exchange = (exchange === null) ? '' : exchange.toUpperCase().trim(); 
+    let hasExchange = current.exchanging.filter(curr => curr.includes(exchange));
+    console.log(exchange, hasExchange); 
+    return hasExchange.length > 0 && current.finding.includes(find);  
   }
 
-  //TODO: FIND NEW KEY 
   return (
     <div>
-      {exchanges.map(exchange => contains(exchange, newFind, newExchange) && <Exchange key={Math.random()*100} exchange={exchange} />)}
+      {exchanges.map(exchange => contains(exchange, newFind, newExchange) && <Exchange key={Math.random()*100} exchange={exchange}/>)}
     </div>
   )
 }
@@ -31,7 +18,7 @@ const Exchange = ({exchange}) => {
   return (
     <div>
       <p>{exchange.user}</p>
-      <h3>{exchange.finding}  |   {exchange.exchanging}</h3>
+      <h3>{exchange.finding}  |   {exchange.exchanging.map(exchange => exchange + ", ")}</h3>
       <p>{exchange.description}</p>
     </div>
   )
--- src/appComponents/MakeNew.js
+++ src/appComponents/MakeNew.js
@@ -1,9 +1,6 @@
-import {useNavigate} from 'react-router-dom';
 import Popup from './Form'
 
 const MakeNew = ({classes}) => {
-  const navigate = useNavigate();
-
   return (
     <div>
       <h3>Can't find what you're looking for?</h3>
